05e8a6dd07445c3a171cc7a0c3c07b432cd428ee
Manoj h j <manojgowdajayaramjyothi1998@gmail.com>
2022-05-16 21:20:30 +0530

    manoj

---
+++ my new program/datatype.java
@@ -0,0 +1,18 @@
+class datatype{
+byte a=-128;
+byte b=127;
+short c=-32768;
+short d=32767;
+int e=-2^31;
+int f=2^30;
+long g=-2^63l;
+long h=2^62l;
+float i=20.2f;
+double j=50.00;
+char k='a';
+boolean l=true;
+boolean m=false;
+void man(){
+System.out.println(a+" "+b+" "+c+" "+d+" "+e+" "+f+" "+g+" "+h+" "+i+" "+j+" "+k+" "+l+" "+m);
+}
+}
