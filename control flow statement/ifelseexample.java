69f90eaf75d931779c76e80de3f5752344721eb2
Manoj h j <manojgowdajayaramjyothi1998@gmail.com>
2022-05-17 23:59:10 +0530

    examples for decision making control flow statement

---
+++ control flow statement/ifelseexample.java
@@ -0,0 +1,12 @@
+class ifelseexample{
+public static void main(String[] args){
+//boolean exampass=true;
+boolean exampass=!true;
+if(exampass){
+System.out.println("manoj clear the exam");
+}
+else{
+System.out.println("manoj fail in exam");
+}
+}
+ }
