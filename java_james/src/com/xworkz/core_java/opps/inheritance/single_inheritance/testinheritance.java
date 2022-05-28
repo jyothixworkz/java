161c2e6bd6dd992672afcf6712451f5f114ff917
91720 <manojgowdajayaramjyothi1998@gmail.com>
2022-05-28 16:57:22 +0530

    example for single inheritance

---
+++ java_james/src/com/xworkz/core_java/opps/inheritance/single_inheritance/testinheritance.java
@@ -0,0 +1,12 @@
+package com.xworkz.core_java.opps.inheritance.single_inheritance;
+
+public class testinheritance {
+	public static void main(String[] args) {
+		parrot pat=new parrot();
+		pat.feeding();
+		pat.flying();
+		pat.egg();
+	}
+
+
+}
