8817952bee256c65031952a629270ffb25f448b9
Manoj h j <manojgowdajayaramjyothi1998@gmail.com>
2022-06-07 15:44:21 +0530

    source file structeres

---
+++ java source file structure/K.java
@@ -0,0 +1,8 @@
+class X{
+}
+class V{
+}
+public class K {
+}
+class F{
+}
---
+++ java source file structure/ashamam.java
@@ -0,0 +1,13 @@
+class N{
+public static void main(String[] args){
+System.out.println("N CLASS MAIN");
+}
+}
+class M
+{
+public static void main(String[] args){
+System.out.println("M CLASS MAIN");
+}
+}
+class R {
+}
