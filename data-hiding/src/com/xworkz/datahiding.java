f76e8d309194a7b4b9c97f7cd62dfec561498b99
91720 <manojgowdajayaramjyothi1998@gmail.com>
2022-06-26 18:58:53 +0530

    object type casting

---
+++ data-hiding/src/com/xworkz/datahiding.java
@@ -0,0 +1,25 @@
+package com.xworkz;
+
+public class datahiding {
+
+	private double ballance;
+	private double getballance() {
+		
+		return 0;
+			
+		
+	}
+}
+class datahidingtest{
+	public static void main(String[] args) {
+		datahidingtest data=new datahidingtest();
+		 double r=data.getballance();
+		 System.out.println(r);
+	}
+
+	//private double getballance() {
+		// TODO Auto-generated method stub
+		//return 0;
+
+	}
+}
---
+++ object_type_casting/src/com/xworkz/test.java
@@ -0,0 +1,11 @@
+package com.xworkz;
+
+public class test {
+	public static void main(String[] args) {
+		bird bird = new peacock();
+		peacock peacock =(peacock)bird;
+		bird bird1 = new bird();
+ peacock peacock1 = (peacock)bird;
+	}
+
+}
