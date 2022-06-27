f3b4ea2748854077bb4896569e8b1788f302657d
91720 <manojgowdajayaramjyothi1998@gmail.com>
2022-06-27 19:07:25 +0530

    object explicit type casting

---
+++ objecttypecasting/src/com/xworkz/explicittypecasting/ExplicitTypeCasting.java
@@ -0,0 +1,14 @@
+package com.xworkz.explicittypecasting;
+
+public class ExplicitTypeCasting {
+	public static void main(String[] args) {
+		Object O=new ExplicitTypeCasting();
+		ExplicitTypeCasting  E=(ExplicitTypeCasting)O;
+		System.out.println(E);
+		/*Object O=new Object();
+		System.out.println(O);
+		ExplicitTypeCasting E=new ExplicitTypeCasting();
+		System.out.println(E);*/
+	}
+
+}
