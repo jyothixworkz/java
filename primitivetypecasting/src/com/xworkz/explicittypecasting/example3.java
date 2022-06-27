2d64d5cd1ae2949c848a4a512313301265c0d243
91720 <manojgowdajayaramjyothi1998@gmail.com>
2022-06-27 19:37:54 +0530

    examples for explicit type casting

---
+++ primitivetypecasting/src/com/xworkz/explicittypecasting/example3.java
@@ -0,0 +1,11 @@
+package com.xworkz.explicittypecasting;
+
+public class example3 {
+
+	public static void main(String[] args) {
+		long L=1500000l;
+		short S=(short)L;
+		System.out.println(S);//-7328
+	}
+
+}
